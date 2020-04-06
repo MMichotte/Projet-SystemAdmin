c154d359d7c7b1277b3dd6d5d891e9684598be7b
momo007Dev <m.valentin@students.ephec.be>
2020-04-06 14:22:17 +0200

    suite mail

---
+++ VPS-Olivier/Mail/config-sql/create-users.sql
@@ -0,0 +1,29 @@
+INSERT INTO `messagerie`.`virtual_domains`
+(`id` ,`name`)
+VALUES
+('1', 'wt2-5.ephec-ti.be');
+
+INSERT INTO `messagerie`.`virtual_users`
+(`id`, `domain_id`, `password` , `email`, `maildir`)
+VALUES
+('1', '1', ENCRYPT('morgan007', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'morgan@wt2-5.ephec-ti.be', 'wt2-5.ephec-ti.be/morgan/');
+
+INSERT INTO `messagerie`.`virtual_users`
+(`id`, `domain_id`, `password` , `email`, `maildir`)
+VALUES
+('2', '1', ENCRYPT('olivier007', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'olivier@wt2-5.ephec-ti.be', 'wt2-5.ephec-ti.be/olivier/');
+
+INSERT INTO `messagerie`.`virtual_users`
+(`id`, `domain_id`, `password` , `email`, `maildir`)
+VALUES
+('3', '1', ENCRYPT('martin007', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'martin@wt2-5.ephec-ti.be', 'wt2-5.ephec-ti.be/martin/');
+
+INSERT INTO `messagerie`.`virtual_users`
+(`id`, `domain_id`, `password` , `email`, `maildir`)
+VALUES
+('4', '1', ENCRYPT('b2b007', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'b2b@wt2-5.ephec-ti.be', 'wt2-5.ephec-ti.be/commerciaux/');
+
+INSERT INTO `messagerie`.`virtual_users`
+(`id`, `domain_id`, `password` , `email`, `maildir`)
+VALUES
+('5', '1', ENCRYPT('contact007', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'contact@wt2-5.ephec-ti.be', 'wt2-5.ephec-ti.be/secretaria/');
