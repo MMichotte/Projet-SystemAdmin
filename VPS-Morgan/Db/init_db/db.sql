223eefb51dbc45b9c8bf7b3324b5a44ee4cbcee3
momo007Dev <m.valentin@students.ephec.be>
2020-04-28 15:32:14 +0200

    vps-morgan users ui improved + db fix

--- VPS-Morgan/Db/init_db/db.sql
+++ VPS-Morgan/Db/init_db/db.sql
@@ -1,12 +1,10 @@
 
---CREATE USER 'woody_admin'@'%' IDENTIFIED BY 'admin';
 CREATE USER 'woody_admin'@'localhost' IDENTIFIED BY 'admin';
 CREATE USER 'woody_admin'@'172.16.0.1' IDENTIFIED BY 'admin';
 CREATE USER 'woody_admin'@'51.178.40.108' IDENTIFIED BY 'admin';
 
 CREATE DATABASE woodytoys_db;
 
--- GRANT ALL PRIVILEGES ON woodytoys_db.* TO 'woody_admin'@'%';
 GRANT ALL PRIVILEGES ON woodytoys_db.* TO 'woody_admin'@'localhost';
 GRANT ALL PRIVILEGES ON woodytoys_db.* TO 'woody_admin'@'172.16.0.1';
 GRANT ALL PRIVILEGES ON woodytoys_db.* TO 'woody_admin'@'51.178.40.108';
